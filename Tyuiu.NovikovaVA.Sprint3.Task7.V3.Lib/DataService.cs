dd20eccb7b0120d3c35a617744b2fd898abfaf7f
NovikovaValeriya <valeriya_novikova_06@mail.ru>
2024-10-27 15:24:27 +0500

    Task7

---
+++ Tyuiu.NovikovaVA.Sprint3.Task7.V3.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.NovikovaVA.Sprint3.Task7.V3.Lib
+{
+    public class DataService : ISprint3Task7V3
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    valueArray[count] = 0;
+                    count++;
+                    continue;
+                }
+                else
+                {
+                    y = Math.Round((3 * x - 1.5) / (Math.Sin(x) - 3 + x) + 2, 2);
+                    valueArray[count] = y;
+                    count++;
+                }
+            }
+            return valueArray;
+
+        }
+    }
+}
